c40767b6760b9663cf7a70e333a421e7db81b042
James Sturtevant <jstur@microsoft.com>
2020-07-27 12:12:25 -0700

    clean up script

--- extensions/gmsa-webhook/v1/install-gmsa-webhook.sh
+++ extensions/gmsa-webhook/v1/install-gmsa-webhook.sh
@@ -4,13 +4,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-tempFolder=$(mktemp -d -t "gmsa-install")
-curl -L -o $tempFolder/deploy-gmsa-webhook.sh https://raw.githubusercontent.com/kubernetes-sigs/windows-gmsa/master/admission-webhook/deploy/deploy-gmsa-webhook.sh 
+gmsaInstallFile="https://raw.githubusercontent.com/kubernetes-sigs/windows-gmsa/master/admission-webhook/deploy/deploy-gmsa-webhook.sh"
 
+echo "$(date -R): getting latest gmsa install file: $gmsaInstallFile"
+tempFolder=$(mktemp -d -t "gmsa-installXXXX") 
+curl -L -o $tempFolder/deploy-gmsa-webhook.sh $gmsaInstallFile
+chmod +x $tempFolder/deploy-gmsa-webhook.sh
+
+echo "$(date -R): running installer"
 export KUBECONFIG="$(find /home/*/.kube/config)"
 $tempFolder/deploy-gmsa-webhook.sh --file "$tempFolder/manifests.yml" \
 	--name "gmsa-webhook" \
 	--namespace "gmsa-webhook" \
 	--certs-dir "$tempFolder/certs" \
 	--tolerate-master
-
+echo "$(date -R): gmsa installed"
